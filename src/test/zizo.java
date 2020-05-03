159de21606ccc019dbdc1c5eb2aaa7332ac22995
Abdelaziz Elgeziry <abdelazizadel99@gmail.com>
2020-05-03 14:51:31 +0200

    Added the project

---
+++ src/test/zizo.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class zizo {
+
+	public static void main(String[] args) {
+		
+		HashSet<String> set = new HashSet<String>();
+		set.add("ziad");
+		System.out.println(set.contains("ziad"));
+		
+	}
+	
+}
